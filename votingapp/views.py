d0079b6a7a8314d5316056aef03c2f6f23eecc3a
Patricio Pagano <patricio.pagano@paradigma.com.ar>
2022-05-31 11:58:10 -0300

    Adding suggestions during search bar with datalist

--- votingapp/views.py
+++ votingapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
 
@@ -9,4 +10,8 @@ def index(request):
         "arr":arr
     }
     return render(request,"index.html",context=mydict)
+
+def getquery(request):
+    query=request.GET['languages']
+    return HttpResponse(query)
 
