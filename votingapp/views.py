efd5e86cbdaf0893a3b1ab454dd9c067d4f889fb
Patricio Pagano <patricio.pagano@paradigma.com.ar>
2022-05-31 11:48:52 -0300

    Adding list + styles

--- votingapp/views.py
+++ votingapp/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,"index.html")
+    arr=['Java','Python','Java','C','C++','DotNET','JavaScript','PHP','Swift','SQL','R','D','Ruby','Delphi','Objective-C','Go','Java','Visual Basic','Perl','MATLAB',]
+
+    mydict={
+        "arr":arr
+    }
+    return render(request,"index.html",context=mydict)
 
