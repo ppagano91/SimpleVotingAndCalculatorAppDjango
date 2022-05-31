442d86c7ba026ce04c288a23ddf87ba1abde7456
Patricio Pagano <patricio.pagano@paradigma.com.ar>
2022-05-31 10:30:16 -0300

    add instructions and def submitquery

--- calculatorapp/views.py
+++ calculatorapp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
 def index(request):
     return render(request,"index.html")
+
+def submitquery(request):
+    query = request.GET['query']
+    json={
+        'query':query
+    }
+
+    return HttpResponse(query)
+    return JsonResponse(json)
